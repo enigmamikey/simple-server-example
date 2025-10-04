522b23bab3db75c0a71161fc44fa3e3beaaa2229
Michael Burgher <mikeburgher2@gmail.com>
2025-10-04 13:10:10 -0600

    function-switch-.gitignoreTest

--- server.js
+++ server.js
@@ -4,75 +4,86 @@ const url = require('url');
 const querystring = require('querystring');
 const figlet = require('figlet')
 
+function sendResponse(res, data, contentType = 'text/html') {
+  res.writeHead(200, {'Content-Type': contentType})
+  res.write(data)
+  res.end()
+}
+
 const server = http.createServer((req, res) => {
   const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
   console.log(page);
-  if (page == '/') {
-    fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/otherpage') {
-    fs.readFile('otherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/otherotherpage') {
-    fs.readFile('otherotherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/api') {
-    if('student' in params){
-      if(params['student']== 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "leon",
-          status: "Boss Man",
-          currentOccupation: "Baller"
+  switch(page) {
+    case '/': {
+      fs.readFile('index.html', function(err, data) {
+        sendResponse(res,data)
+      });
+      break;
+    }
+    case '/otherpage': {
+      fs.readFile('otherpage.html', function(err, data) {
+      sendResponse(res,data)
+      });
+      break;
+    }
+    case '/otherotherpage': {
+      fs.readFile('otherotherpage.html', function(err, data) {
+      sendResponse(res,data)
+      });
+      break;
+    }
+    case '/api': {
+      if('student' in params){
+        if(params['student']== 'leon'){
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          const objToJson = {
+            name: "leon",
+            status: "Boss Man",
+            currentOccupation: "Baller"
+          }
+          res.end(JSON.stringify(objToJson));
+        }//student = leon
+        else if(params['student'] != 'leon'){
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          const objToJson = {
+            name: "unknown",
+            status: "unknown",
+            currentOccupation: "unknown"
+          }
+          res.end(JSON.stringify(objToJson));
+        }//student != leon
+      }//student if
+      break;
+    }  
+    case '/css/style.css': {
+      fs.readFile('css/style.css', function(err, data) {
+        res.write(data);
+        res.end();
+      });
+      break;
+    }
+    case '/js/main.js': {
+      fs.readFile('js/main.js', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/javascript'});
+        res.write(data);
+        res.end();
+      });
+      break;
+    }
+    default: {
+      figlet('404!!', function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
         }
-        res.end(JSON.stringify(objToJson));
-      }//student = leon
-      else if(params['student'] != 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "unknown",
-          status: "unknown",
-          currentOccupation: "unknown"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student != leon
-    }//student if
-  }//else if
-  else if (page == '/css/style.css'){
-    fs.readFile('css/style.css', function(err, data) {
-      res.write(data);
-      res.end();
-    });
-  }else if (page == '/js/main.js'){
-    fs.readFile('js/main.js', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
-      res.write(data);
-      res.end();
-    });
-  }else{
-    figlet('404!!', function(err, data) {
-      if (err) {
-          console.log('Something went wrong...');
-          console.dir(err);
-          return;
-      }
-      res.write(data);
-      res.end();
-    });
+        res.write(data);
+        res.end();
+      });
+    }
   }
+    
 });
 
 server.listen(8000);
